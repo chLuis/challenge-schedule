cfd59706f30a6f4bbb51895038cf39e9dc4f6f3b
Luis Chrestia <chrestia.luis@gmail.com>
2024-07-27 01:02:45 -0300

    fix, localstorage not defined

--- stores/storage/local-storage.ts
+++ stores/storage/local-storage.ts
@@ -2,16 +2,23 @@ import { createJSONStorage } from "zustand/middleware";
 
 const storageApi = {
   getItem: function (name : string) {
-    const data = localStorage.getItem(name)
-    return data
+    if (typeof localStorage !== "undefined") {
+      const data = localStorage.getItem(name);
+      return data;
+    }
+    return null;
   },
 
   setItem: function (name: string, value: any) {
-    localStorage.setItem(name, value);
+    if (typeof localStorage !== "undefined") {
+      localStorage.setItem(name, value);
+    }
   },
 
   removeItem: function (name: string) {
-    localStorage.removeItem(name);
+    if (typeof localStorage !== "undefined") {
+      localStorage.removeItem(name);
+    }
   }
 }
 
